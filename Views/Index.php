14ac9d0b5bf5fd7a480a8f7a448bb9dfef0a839c
Janek Bevendorff <contact@refining-linux.org>
2011-07-13 00:14:07 +0200

    Filled README file with text, added LICENSE file

--- Views/Index.php
+++ Views/Index.php
@@ -7,7 +7,7 @@
 <body>
     <div>
         <h1>Privacy for Buttons v<?php print PFB_CONFIG_APP_VERSION; ?></h1>
-        <p>Privacy for Buttons helps you to protect your visitor's providing full-featured social media buttons without third-party tracking.</p>
+        <p>Privacy for Buttons helps you to protect your visitor's privacy providing full-featured social media buttons without third-party tracking.</p>
         <p>It is free software and released under the <a href="http://www.opensource.org/licenses/bsd-license.php">BSD license</a>.</p>
     </div>
 </body>
